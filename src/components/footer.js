c54338c96cd5d92ec449dfe59bcc13087fd4b6d0
Ayush Mandowara <ayushxx7@gmail.com>
2020-08-26 21:54:19 +0530

    fix: footer style updated. links colored. icon added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,21 @@
 import React from "react"
+import {Link} from "gatsby"
+import {
+  CgGitFork,
+  AiFillStar,
+  FaGithub,
+}
+  from "react-icons/fa"
+
+
 const Footer = () => {
   return (
     <div>
       <h4>
-        To thank the author, give a star to: <a href="https://github.com/ayushxx7/ayush-mandowara-blog"> this repo.</a>
+        Thank the
+        <Link to={'https://github.com/ayushxx7'} style={{color: "#f33"}} > author</Link>. Star or Fork <Link to={'https://github.com/ayushxx7/ayush-mandowara-blog'} style={{color: "#9cdcfe"}} > this blog. <FaGithub color="#FFFFFF" size="25" /></Link>
       </h4>
-    </div>
+    </div >
   )
 }
 
