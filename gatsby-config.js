f5a9dffaabd9797ef17aabfe34d24ccff3c0c2d2
Ayush Mandowara <ayushxx7@gmail.com>
2020-10-25 15:31:59 +0530

    feat(Blog): upgrade dependencies
    update all packages using `npm upgrade`
    add `gatsby-remark-images` config in gatsby-config.js for inline image
    support in .md documents

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,5 +64,25 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
 }
