ba47e1c207d5996510067bbe0ce0553897b88b37
Ayush Mandowara <ayushxx7@gmail.com>
2021-02-14 18:26:36 +0530

    feat(blog): external links open in new tab
    add plugin gatsby-remark-external-links and update config to open
    external links in new tab, along with adding the "nofollow" tag to it.

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow"
+            }
+          },
+          {
             resolve: `gatsby-remark-table-of-contents`,
             options: {
               exclude: "Table of Contents",
